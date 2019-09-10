3197655dcda2edf99f187bd54bdc9e120e6dfcea
raotnameh <raotnameh@gmail.com>
2019-09-11 03:29:02 +0530

    updated

--- HW1/main.py
+++ HW1/main.py
@@ -4,7 +4,7 @@ import sys
 
 class bandits():
 	def __init__(self,no_bandit = 4, eps = 0, no_iter = 1000, 
-						seed = 123456, stationary = True, starting_reward = 0
+						seed = 123456, stationary = True, starting_reward = 5
 						, weighted_aver = False, alpha = 0.1):
         
 		np.random.seed(seed)
@@ -26,13 +26,13 @@ class bandits():
 
 # Prob/Mean of winning "1" for each bandit
 	def individual_prob(self,sigma=0):
-		return np.random.uniform(-5,5,size = self.no_bandit)
+		return np.random.uniform(sigma,1,size = self.no_bandit)
 
  # True Reward at a particular time step or "1" and "0" at each time step or Distribution dependent reward
 	def distri_depen_rewa(self,prob):
 		out = []
 		for i in prob:
-			out.append(np.random.normal(i,10))
+			out.append(np.random.normal(i,1))
 
 		return out # list of rewards for each arm/bamdit at a particular time step
 
@@ -57,14 +57,12 @@ class bandits():
 		optimal_action = 0
 		optimal_action_total = []
 		prob = self.individual_prob()
+		print("True mean of each arm", prob)
 		for i in range(self.no_iter):
 
-			if self.stationary == False:
-				prob = self.individual_prob()
-
-
 			action = self.action()
-			reward = self.distri_depen_rewa(prob)[action]
+			reward = self.distri_depen_rewa(prob)
+			reward_a = reward[action]
 
 			self.steps += 1
 			self.individual_steps[action] +=1 
@@ -72,17 +70,22 @@ class bandits():
 			self.reward_after_each_step.append((self.total_reward))
 
 			if self.weighted_aver == True:
-				self.total_reward += (reward - (self.total_reward))*(self.alpha)
-				self.individual_mean[action] += (reward - self.individual_mean[action])/(self.individual_steps[action])
+				self.total_reward += (reward_a - (self.total_reward))*(self.alpha)
+				self.individual_mean[action] += (reward_a - self.individual_mean[action])/(self.individual_steps[action])
 
 			else: 
-				self.total_reward += (reward - (self.total_reward))/(self.steps)
-				self.individual_mean[action] += (reward - self.individual_mean[action])/(self.individual_steps[action])
+				self.total_reward += (reward_a - (self.total_reward))/(self.steps)
+				self.individual_mean[action] += (reward_a - self.individual_mean[action])/(self.individual_steps[action])
 
-			if self.reward_after_each_step[i-1] <= self.reward_after_each_step[i]:
+			if reward_a >= max(reward):
+				# print("optimal_action")
 				optimal_action +=1
 		    
 			optimal_action_total.append(optimal_action/self.steps)
 		    
+
+			if self.stationary == False:
+				prob = self.individual_prob()
+
 		return self.reward_after_each_step, self.individual_mean, optimal_action_total
         
--- HW1/train.py
+++ HW1/train.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 parser = argparse.ArgumentParser(description = "train file")
 parser.add_argument("--seed", type = int, default = 123456789, help = "enter the value of seed")
 parser.add_argument("--bandit", type=int, default = 10, help = "enter the no of bandits")
-parser.add_argument("--iter", type = int, default = 1000, help = "enter the no. of iterations")
-parser.add_argument("--reward", type = float, default = 0, help = "enter the starting reward")
-parser.add_argument("--eps", type = float, default = 0.1, help = "enter the value of epsilon")
+parser.add_argument("--iter", type = int, default = 100, help = "enter the no. of iterations")
+parser.add_argument("--reward", type = float, default = 5, help = "enter the starting reward")
+parser.add_argument("--eps", type = float, default = 0, help = "enter the value of epsilon")
 parser.add_argument("--stat", action = 'store_false', help = "enter the environment variable")
 parser.add_argument("--alpha", type = float, default = 0.1, help = "enter the value of alpha for weighted_aver")
 parser.add_argument("--weighted", action = 'store_true', help = "enter if weighted_aver is true")
@@ -31,13 +31,15 @@ if __name__ == '__main__':
 							seed = args.seed, stationary = args.stat, starting_reward = args.reward
 							, weighted_aver = args.weighted, alpha = args.alpha)
 
-	reward, mean, action= prob.forward()
+	reward_at_Each_step, indiv_prob_dist , optim_action_at_each_step= prob.forward()
+
+	print("predicted mean of each arm", indiv_prob_dist)
 
 
-	print("Average total rewardafter "+str(args.iter)+" steps is : ",reward[-1])
+	print("Average total rewardafter "+str(args.iter)+" steps is : ",reward_at_Each_step[-1])
 
 
-	print("optimal_action after "+str(args.iter)+" steps is : ", action[-1])
-	plt.plot(reward)
-	plt.plot(action)
+	print("percentage of optimal action after "+str(args.iter)+" steps is : ", optim_action_at_each_step[-1])
+	# plt.plot(reward_at_Each_step)
+	plt.plot(optim_action_at_each_step)
 	plt.show() 
